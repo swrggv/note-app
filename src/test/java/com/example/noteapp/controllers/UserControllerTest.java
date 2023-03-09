0753c353f0513a5371c8e1204a0ba5d29b04efba
Sash <notsadako@yandex.ru>
2023-03-09 21:54:38 +0300

    add tests

--- src/test/java/com/example/noteapp/controllers/UserControllerTest.java
+++ src/test/java/com/example/noteapp/controllers/UserControllerTest.java
@@ -1,16 +1,69 @@
 package com.example.noteapp.controllers;
 
+import com.example.noteapp.models.dto.UserDto;
+import com.example.noteapp.services.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.nio.charset.StandardCharsets;
 
+import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = UserController.class)
 class UserControllerTest {
+    @MockBean
+    private UserService userService;
 
-    @Test
-    void addUser() {
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private ObjectMapper mapper;
+    private UserDto userDto;
+
+    @BeforeEach
+    void setUp() {
+        userDto = new UserDto(1, "user");
     }
 
     @Test
-    void getUser() {
+    void addUser() throws Exception {
+        Mockito.when(userService.addUser(any()))
+                .thenReturn(userDto);
+        mvc.perform(post("/users")
+                        .content(mapper.writeValueAsString(userDto))
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding(StandardCharsets.UTF_8))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(userDto.getId()), Long.class))
+                .andExpect(jsonPath("$.name", is(userDto.getName()), String.class));
+    }
+
+    @Test
+    void getUser() throws Exception {
+        Mockito.when(userService.getUser(anyLong()))
+                .thenReturn(userDto);
+        mvc.perform(get("/users/{idUser}", userDto.getId())
+                        .content(mapper.writeValueAsString(userDto))
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding(StandardCharsets.UTF_8))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(userDto.getId()), Long.class))
+                .andExpect(jsonPath("$.name", is(userDto.getName()), String.class));
     }
 }
